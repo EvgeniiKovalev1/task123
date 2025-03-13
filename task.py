325a740ea9bc8e7e1da7130124ffecaf4af8bd00
EvgeniiKovalev1 <kovalevg05@mail.ru>
2025-03-13 20:47:22 +0300

    fix

--- task.py
+++ task.py
@@ -1,15 +1,17 @@
 import os
 import socket
 import logging
+import threading
 
 STATIC_DIR = "static"
 
 logging.basicConfig(filename="server.log",
 level=logging.INFO,
-format="%(asctime)s - %(levelname)s - %(message)s")
+format="%(asctime)s - %(levelname)s - %(message)s",
+encoding='utf-8')
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('localhost', 8000))
-server_socket.listen(123)
+server_socket.listen(1)
 logging.info("Сервер запущен")
 
 def get_content_type(name):
@@ -30,15 +32,20 @@ while True:
     logging.info(f"Запрос от {client_address}")
     if method == "GET":
         file_path = os.path.join(STATIC_DIR, path.lstrip("/"))
-        try:
-            with open(file_path, "rb") as file:
-                response_method = file.read()
-                content_type = get_content_type(file_path)
-                response = f"HTTP/1.1 200 OK\r\nContent-Length: {len(response_method)}\r\nContent-Type: {content_type}\r\n\r\n{response_method}/r/nConnection: close"
-                logging.info(f"Успешный GET-запрос: {path} {len(response_method)} байт")
-        except Error:
-            response = f"HTTP/1.1 404 Not Found"
-            logging.error(f"{file_path} не найден")
+        if os.path.exists(file_path) and os.path.isfile(file_path):
+            try:
+                with open(file_path, "rb") as file:
+                    response_method = file.read()
+                    content_type = get_content_type(file_path)
+                    response = f"HTTP/1.1 200 OK\r\nContent-Length: {len(response_method)}\r\nContent-Type: {content_type}\r\n\r\n{response_method}/r/nConnection: close"
+                    logging.info(f"Успешный GET-запрос к статике: {path} {len(response_method)} байт")
+            except FileNotFoundError:
+                response_body = "File not found"
+                response = f"HTTP/1.1 404 Not Found\r\nContent-Length: {len(response_body)}\r\nContent-Type: text/plain\r\n\r\n{response_body}\r\nConnection: close"
+        else:
+            response_method = "GET"
+            response = f"HTTP/1.1 200 OK\r\nContent-Length: {len(response_method)}\r\nContent-Type: text/plain\r\n\r\n{response_method}/r/nConnection: close"
+            logging.info(f"Успешный GET-запрос: {path} {len(response_method)} байт")
     elif method == "POST":
         response_method = "POST"
         response = f"HTTP/1.1 200 OK\r\nContent-Length: {len(response_method)}\r\nContent-Type: text/plain\r\n\r\n{response_method}/r/nConnection: close"
@@ -48,5 +55,7 @@ while True:
         response = f"HTTP/1.1 405 \r\nContent-Length: {len(response_method)}\r\nContent-Type: text/plain\r\n\r\n{response_method}/r/nConnection: close"
         logging.error(f"{method}-Не поддерживается")
 
-    client_socket.sendall(response)
-    client_socket.close()
+while True:
+    client_socket, client_address = server_socket.accept()
+    thread = threading.Thread(target=get_content_type, args=(client_socket, client_address))
+    thread.start()
